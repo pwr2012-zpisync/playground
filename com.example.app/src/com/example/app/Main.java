f2500e655a7ad09be9ddfb565541335ddc2a1886
Tomasz Wisniewski <corckie@gmail.com>
2012-10-18 10:21:59 +0200

    test
    test

--- com.example.app/src/com/example/app/Main.java
+++ com.example.app/src/com/example/app/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world from marcin after merge!");
+		System.out.println("Hello world from marcin after merge! elo elo");
 	}
 
 }
